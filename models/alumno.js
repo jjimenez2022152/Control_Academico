23e3401542d9dc435f54ffffd3fe58c9ebe1b59c
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2024-02-13 15:32:35 -0600

    se crea modelo de alumno

---
+++ models/alumno.js
@@ -0,0 +1,35 @@
+const { Schema, model} = require('mongoose');
+
+const UsuarioSchema = Schema({
+    nombre:{
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo:{
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    role:{
+        type: String,
+        require: true,
+        enum: ["STUDENT_ROLE"]
+    },
+    google:{
+        type: Boolean,
+        default: false
+    }   
+});
+
+AlumnoSchema.methods.toJSON = function(){
+    const{ __v, password, _id, ...alumno} = this.toObject();
+    alumno.uid = _id;
+    return alumno;
+};
+
+module.exports = model('Alumno', AlumnoSchema);
+
